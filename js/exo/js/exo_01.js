fe0a8ddeeb865d33ed5419750689bf9420b7ccdb
elies <elieskedim23@gmail.com>
2018-07-16 14:34:42 +0200

    cours js

---
+++ js/exo/js/exo_01.js
@@ -0,0 +1,4 @@
+var prix = Number.parseInt(prompt("Entrez un chiffre."));
+if (Number.isInteger(prix)){
+    console.log("Votre article vaux " + prix + " HT et " + prix * 1.20 + " TTC.");
+}else{console.log("Tu veux faire ton Malin ?");}
