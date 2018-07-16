f9ca2e0495a840d8093bfd4032e7f61286d180c2
elies <elieskedim23@gmail.com>
2018-07-16 16:53:54 +0200

    js

--- js/exo/js/exo_01.js
+++ js/exo/js/exo_01.js
@@ -1,4 +1,6 @@
 var prix = Number.parseInt(prompt("Entrez un chiffre."));
 if (Number.isInteger(prix)){
     console.log("Votre article vaux " + prix + " HT et " + prix * 1.20 + " TTC.");
-}else{console.log("Tu veux faire ton Malin ?");}
+}else{
+    console.log("Tu veux faire ton Malin ?");
+}
---
+++ js/exo/js/exo_02.js
@@ -0,0 +1,2 @@
+var age = prompt("etes-vous majeur ?");
+age < 18 ? document.location.href = "page2.html" : alert("Tu est bien majeur enjaille tois mon pote !");
